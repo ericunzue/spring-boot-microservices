3608e9e21794c8a853e0be5d874614d62505e51f
ericunzue <ericunzue@gmail.com>
2024-02-03 15:30:08 -0300

    feat:add discovery server

--- microservices-new/order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
+++ microservices-new/order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
@@ -2,10 +2,10 @@ package com.microservice.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
